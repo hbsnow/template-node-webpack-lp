fa2f5956112eaf9855e260a67f041ffb8210e996
hbsnow <yuki.4uing@gmail.com>
2020-02-21 09:44:35 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+  ],
+  env: {
+    browser: true,
+    node: true,
+    es2020: true
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+  },
+  parserOptions: {
+    sourceType: "module"
+  }
+}
