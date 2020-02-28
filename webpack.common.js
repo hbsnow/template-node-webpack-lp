c7e138665f986010a60fdfa6e5fa262ad702f811
hbsnow <yuki.4uing@gmail.com>
2020-02-29 01:22:25 +0900

    feat: scssのビルド設定追加

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,37 @@
+const path = require('path')
+const MiniCssExtract = require('mini-css-extract-plugin')
+const FixStyleOnlyEntries = require('webpack-fix-style-only-entries')
+
 module.exports = {
-  entry: `./src/assets/js/main.js`,
-  output: {
-    path: `${__dirname}/dist/assets/js`,
-    filename: 'main.js',
+  entry: {
+    main: `./src/assets/js/main.js`,
+    'main.css': `./src/assets/css/main.scss`,
   },
+  output: {
+    filename: 'assets/js/[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules\/(?!(dom7|ssr-window|swiper)\/).*/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
+        test: /\.scss/,
+        use: [MiniCssExtract.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new FixStyleOnlyEntries(),
+    new MiniCssExtract({
+      filename: 'assets/css/[name]',
+    }),
+  ],
 }
