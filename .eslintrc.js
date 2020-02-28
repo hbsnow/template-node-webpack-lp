6fd68dc035790be8ed10a761e156c26936c16e05
hbsnow <yuki.4uing@gmail.com>
2020-02-28 09:53:48 +0900

    feat: add stylelint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,15 @@
 module.exports = {
-  extends: [
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   env: {
     browser: true,
     node: true,
-    es2020: true
+    es2020: true,
   },
   parserOptions: {
     ecmaVersion: 2020,
   },
-  rules: {
-  },
+  rules: {},
   parserOptions: {
-    sourceType: "module"
-  }
+    sourceType: 'module',
+  },
 }
